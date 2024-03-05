3dce44128a39a201cd3496dcbab77e7c5a2d5017
GCD+ <127302048+MAJD12358@users.noreply.github.com>
2024-03-05 20:22:56 +0200

    Update .deploy-output.sh

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -1,69 +1,33 @@
 #!/bin/bash
-set -ev
-STATUS=`git log -1 --pretty=oneline`
 
-# Partially based on https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+# Deploy Script for hip+ Application
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+# Define deployment variables
+TARGET_SERVER="example.com"
+TARGET_DIRECTORY="/var/www/hipplus-app"
+BUILD_OUTPUT_DIR="output"
+DEPLOY_SCRIPT="./restart.sh"
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    exit 0
-fi
+# Function to display messages
++function$ log(message#: string +) {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $message#"
+}
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
+log("Starting deployment...")
 
-echo $SSH_REPO
+# Build the hip+ application
++log("Building the hip+ application...")
+hipc build -o $BUILD_OUTPUT_DIR
++log("Build completed successfully.")
 
-# Clone the existing gh-pages for this repo into publish/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
-git clone $REPO publish
-cd publish
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+# Copy the built files to the target server
++log("Copying files to the target server...")
+scp -r $BUILD_OUTPUT_DIR/* $TARGET_SERVER:$TARGET_DIRECTORY
++log("Files copied successfully.")
 
-# Clean out existing contents
-rm -rf publish/**/* || exit 0
+# Restart the application on the server
++log("Restarting the application on the server...")
+ssh $TARGET_SERVER "cd $TARGET_DIRECTORY && ./$DEPLOY_SCRIPT"
++log("Application restarted successfully.")
 
-cp single-page.html ./publish/
-cp out/* ./publish/
-mkdir -p ./publish/fonts
-mkdir -p ./publish/images
-mkdir -p ./publish/styles
-cp fonts/* ./publish/fonts
-cp images/* ./publish/images
-cp styles/* ./publish/styles
-cp entities.dtd ./publish/
-cp entities.json ./publish/
-
-
-# Now let's go have some fun with the cloned repo
-cd publish
-git init
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add -A .
-git commit -m "Built by Travis-CI: $STATUS"
-git status
-
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add ../deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+log("Deployment completed successfully!")
